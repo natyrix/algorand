c62e0363892d73ea9c5980bc2a012c61380c8ce7
Natnael Melese <natrix277@gmail.com>
2022-09-28 18:44:17 -0400

    Model created on backend

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    address = models.CharField(max_length=255)
+
+class Assets(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    asset_index = models.CharField(max_length=255)
