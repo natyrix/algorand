280bde3ebc2c91ebc23100d4c7f72bc1ef0ac9ab
Natnael Melese <natrix277@gmail.com>
2022-10-01 22:15:09 -0400

    Back end final touches

--- api/models.py
+++ api/models.py
@@ -17,8 +17,10 @@ class Account(models.Model):
 class Assets(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
     asset_index = models.CharField(max_length=255)
+    asset_name = models.CharField(max_length=255, default="None")
     asset_status = models.BooleanField(default=False)
     image_url = models.CharField(max_length=255, default="None")
+    ipfs_url = models.CharField(max_length=255, default="None")
 
     # def __str__(self):
     #    return self.first_name + self.last_name
--- api/urls.py
+++ api/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('set_asset_index', views.set_asset_index, name='set_asset_index'),
     path('get_all_requests', views.get_all_requests, name='get_all_requests'),
     path('get_all_assets', views.get_all_assets, name='get_all_assets'),
+    path('set_asset_final_status', views.set_asset_final_status, name='set_asset_final_status'),
+    path('create_trainee_request', views.create_trainee_request, name='create_trainee_request'),
     path('get_assets_trainee', views.get_assets_trainee, name='get_assets_trainee'),
 ]
